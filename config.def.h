30ddf127c3e6e5c2f6bc8c51e8ce0eb19c426eb9
hugenpwe17 <820029885@qq.com>
2022-05-22 14:00:08 +0800

    update: enable some shortcut

--- config.def.h
+++ config.def.h
@@ -151,8 +151,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	// { MODKEY,                       XK_s,      show,           {0} },
-	// { MODKEY,                       XK_h,      hide,           {0} },
+	{ MODKEY|ShiftMask,                       XK_o,      show,           {0} },
+	{ MODKEY,                       XK_o,      hide,           {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
